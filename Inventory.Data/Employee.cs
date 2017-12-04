5307288aef7ad0544fdd28a7ef32b73051305cf3
Mjmarek <monicamarek24@gmail.com>
2017-12-04 17:04:34 -0500

    Added classes for Product table and Employee table

---
+++ Inventory.Data/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data
+{
+    public class Employee
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Key]
+        public int IdNumber { get; set; }
+
+        [Required]
+        public string UserName { get; set; } //foreign key
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Inventory.Data/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Data
+{
+    public class Product
+    {
+        public bool IsFlagged { get; set; }
+
+        [Key]
+        public string Number { get; set; } //item numbers include both letters & numbers
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Location { get; set; } //locations can include combination of letters and numbers
+
+        public string Comments { get; set; }
+
+        [Required]
+        public string UserName { get; set; } //foreign key
+    }
+}
