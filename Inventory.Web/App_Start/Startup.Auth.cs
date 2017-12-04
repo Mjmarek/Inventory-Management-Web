b96d11ff8f715a0904cee41fb02fe32c203b6702
Mjmarek <monicamarek24@gmail.com>
2017-12-04 16:31:37 -0500

    Installed entity framework & added references

--- Inventory.Web/App_Start/Startup.Auth.cs
+++ Inventory.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using Inventory.Web.Models;
+using Inventory.Data;
 
 namespace Inventory.Web
 {
--- Inventory.Web/Controllers/AccountController.cs
+++ Inventory.Web/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Inventory.Web.Models;
+using Inventory.Data;
 
 namespace Inventory.Web.Controllers
 {
