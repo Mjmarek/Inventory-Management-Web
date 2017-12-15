972c135d92e1f0b26163656190ccc5581f2dac42
Mjmarek <monicamarek24@gmail.com>
2017-12-15 16:32:38 -0500

    started adding unit tests

---
+++ Inventory.Tests/InventoryControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Tests
+{
+    [TestClass]
+    public class InventoryControllerTests
+    {
+        [TestMethod]
+        public void Index_Returns_All_Products_In_DB()
+        {
+            var product = Mock.Create<IProduct>();
+
+            Mock.Arrange(() => product.GetProducts()).
+                Returns(new List<ProductListModel>()
+                {
+                    new ProductListModel {}
+                }
+        }
+    }
+}
