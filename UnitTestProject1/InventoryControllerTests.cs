ce187abd0dcfee377c502b47a3c120e3778d294b
Mjmarek <monicamarek24@gmail.com>
2017-12-17 19:16:33 -0500

    Added unit tests

--- UnitTestProject1/InventoryControllerTests.cs
+++ UnitTestProject1/InventoryControllerTests.cs
@@ -5,6 +5,7 @@ using Inventory.Models;
 using Inventory.Web.Controllers;
 using Telerik.JustMock;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace ProductServices.Test
 {
@@ -12,48 +13,20 @@ namespace ProductServices.Test
     public class InventoryControllerTests
     {
         [TestMethod]
-        public void Index_Returns_All_Products()
-        {
-            //Arange
-            var product = Mock.Create<IProduct>();
-
-            Mock.Arrange(() => product.GetProducts()).
-                Returns(new List<ProductListModel>()
-                {
-                    new ProductListModel { ProductId=3, Flag=1, Number="ABC-0123", Name="Alphabet Blocks",
-                            Quantity=0, Location="0602", Comments="2 in backstock", UserName="Manager1"},
-                    new ProductListModel { ProductId=5, Number="ABC-1234", Name="Dictionary",
-                            Quantity=1, Location="02BOOKSA", Comments="Check backstock", UserName="Manager1"},
-                    new ProductListModel { ProductId=9, Number="EFG-2345", Name="Math Puzzle",
-                            Quantity=1, Location="0103", Comments="", UserName="Manager1"}
-
-                }).MustBeCalled();
-            Guid newGuid = new Guid();
-
-            //Act
-            InventoryController controller = new InventoryController(product);
-            ViewResult viewResult = controller.Index();
-            var model = viewResult.Model as IEnumerable<ProductListModel>;
-
-            //Assert
-            Assert.AreEqual(product, 2);
-        }
-
-        [TestMethod()]
-        public void QuoteCreateTest()
+        public void CreateShouldPopulateFlagCategory()
         {
             //Arrange
             InventoryController inventoryController = new InventoryController();
 
             //Act
-            ActionResult result = inventoryController.Create() as ActionResult;
+            ViewResult result = inventoryController.Create() as ViewResult;
 
             //Assert
-            Assert.IsNotNull(result);
+            Assert.AreEqual(3, ((ProductCreateModel)result.Model).FlagCategoryList.Count());
         }
 
-        [TestMethod()]
-        public void QuoteIndexTest()
+        [TestMethod]
+        public void InventoryIndexTest()
         {
             //Arrange
             InventoryController inventoryController = new InventoryController();
