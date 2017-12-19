37fe8cc8809bf9bd6d106db117bdc95f3a7353ae
Mjmarek <monicamarek24@gmail.com>
2017-12-18 19:10:20 -0500

    Added IProductService to Android app

---
+++ Inventory.Mobile/Inventory.Mobile/Contracts/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Mobile.Contracts
+{
+    interface IProductService
+    {
+        Task<bool> Login(string username, string password);
+
+        Task<List<ProductListModel>> GetProductList();
+
+        Task<ProductDetailsModel> GetProductById(int productId);
+
+        Task<bool> CreateProduct(ProductCreateModel product);
+
+        Task<bool> DeleteProduct(int productId);
+    }
+}
