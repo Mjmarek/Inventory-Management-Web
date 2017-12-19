1fb26024a534e9cf1473e4be7729635ed553e68d
Mjmarek <monicamarek24@gmail.com>
2017-12-18 22:54:46 -0500

    Updated connection string

---
+++ Inventory.Mobile/Inventory.Mobile/Models/ProductListModel.cs
@@ -0,0 +1,59 @@
+﻿using Inventory.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Mobile
+{
+    public class ProductListModel
+    {
+        public int ProductId { get; set; }
+
+        public ProductAvailability? Flag { get; set; }
+
+        public string FlagColor => GetFlagColor();
+
+        private string GetFlagColor()
+        {
+            switch (Flag)
+            {
+                case ProductAvailability.Available:
+                    return "#009900";
+                case ProductAvailability.OnOrder:
+                    return "#ffcc00";
+                case ProductAvailability.NotAvailable:
+                    return "#ff0000";
+                default:
+                    return null;
+            }
+        }
+
+        public string FlagCategory => GetFlagCategory();
+
+        private string GetFlagCategory()
+        {
+            switch (Flag)
+            {
+                case ProductAvailability.Available:
+                    return "Available";
+                case ProductAvailability.OnOrder:
+                    return "On Order";
+                case ProductAvailability.NotAvailable:
+                    return "Not Available";
+                default:
+                    return null;
+            }
+        }
+
+        public string Number { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Location { get; set; }
+
+        public string Comments { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ Inventory.Mobile/Inventory.Mobile/Views/ProductDetails.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Inventory.Mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductDetails : ContentPage
+    {
+        private int? _productId = null;
+
+        public ProductDetails(int? productId)
+        {
+            InitializeComponent();
+            _productId = productId;
+            SetupUi();
+        }
+
+        public async void SetupUi()
+        {
+            this.Title = _productId.HasValue ? "Edit Product" : "New Note";
+
+            // If we're creating a new note, disable the Starred switch.
+            if (!_noteId.HasValue) fldIsStarred.IsEnabled = false;
+
+            // If we're editing a note, load the note.
+            if (_noteId.HasValue)
+            {
+                // Add delete option.
+                this.ToolbarItems.Add(new ToolbarItem("Delete", null, async () =>
+                {
+                    // Confirm they want to delete.
+                    if (await DisplayAlert("Well?", "Are you sure you want to delete this note?", "Yep", "Nope"))
+                    {
+                        await App.NoteService.Delete(_noteId.Value).ContinueWith(async task =>
+                        {
+                            if (task.Result)
+                            {
+                                await DisplayAlert("Great!", "The note has been deleted.", "Okie Dokie");
+                                await Navigation.PopAsync(true);
+                            }
+                            else
+                            {
+                                await DisplayAlert("Bummer",
+                                    "The note could not be deleted. Are you sure it's still there?", "Okie Dokie");
+                            }
+                        }, TaskScheduler.FromCurrentSynchronizationContext());
+                    }
+                }));
+
+
+
+
+
+
+
+                ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+    }
+}
